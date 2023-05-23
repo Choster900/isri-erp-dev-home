e4776b45411214d7b949f7c52468eb3914dcaa6a
Administrator <admin@example.com>
2023-05-23 23:20:07 +0000

    Develop

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,11 +6,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title inertia>{{ config('app.name', '') }}</title>
 
-        <link rel="icon" type="image/x-icon" href="https://www.presidencia.gob.sv/wp-content/uploads/2019/06/LogoPagina_Mesa-de-trabajo-1.png">
+        <link rel="icon" type="image/x-icon" href="{{ asset("/img/LogoGobSVSmallSize.png") }}">
         <!-- <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}"> -->
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <!-- <link rel="stylesheet" href="{{ asset("css/font_nunito.css") }}">-->
+        <link rel="stylesheet" type="text/css" href="{{ url('../css/font_nunito.css') }}" >
 
         <!-- Scripts -->
         @routes
