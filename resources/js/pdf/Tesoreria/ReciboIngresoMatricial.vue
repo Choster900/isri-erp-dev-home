f49e33f65f7f2d73fdf3cb711610bb4442f70678
Melvin_hj94 <tec1.unifanalista@isri.gob.sv>
2023-05-29 10:49:18 -0600

    Adding prop letras1 y letras2 in ReciboIngresoMatricial.vue

--- resources/js/pdf/Tesoreria/ReciboIngresoMatricial.vue
+++ resources/js/pdf/Tesoreria/ReciboIngresoMatricial.vue
@@ -234,6 +234,14 @@ export default {
             type: String,
             default: '',
         },
+        letras1: {
+            type: String,
+            default: '',
+        },
+        letras2:{
+            type: String,
+            default: '',
+        }
     },
     data: function () {
         return {
