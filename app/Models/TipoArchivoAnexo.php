9a9316d750f37806655b5bcf799de68db2bd39aa
Sergio Adonay López Mejía <sergio.lopez@ISRI.CENTRAL>
2023-11-01 08:33:23 -0600

    (Feat) new model for Anexos

---
+++ app/Models/TipoArchivoAnexo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoArchivoAnexo extends Model
+{
+    use HasFactory;
+}
