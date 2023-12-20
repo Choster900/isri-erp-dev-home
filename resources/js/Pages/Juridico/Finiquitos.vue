6b8179030f2c91b931679b453db7cfb6c6edaee9
Melvin_hj94 <tec1.unifanalista@isri.gob.sv>
2023-12-20 13:23:59 -0600

    Adding new module

---
+++ resources/js/Pages/Juridico/Finiquitos.vue
@@ -0,0 +1,80 @@
+<template>
+    <Head title="Proceso - Finiquitos" />
+    <AppLayoutVue nameSubModule="Juridico - Finiquitos" :autoPadding="false">
+        <div>
+            <p>holaaa</p>
+        </div>
+    </AppLayoutVue>
+</template>
+
+<script>
+import { Head } from "@inertiajs/vue3";
+import AppLayoutVue from "@/Layouts/AppLayout.vue";
+import moment from 'moment';
+import InputError from "@/Components/InputError.vue";
+import { jsPDF } from "jspdf";
+import { ref, toRefs, computed, onMounted, watch } from 'vue';
+import { usePermissions } from '@/Composables/General/usePermissions.js';
+import DateTimePickerM from "@/Components-ISRI/ComponentsToForms/DateTimePickerM.vue";
+
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
+import axios from 'axios';
+
+export default {
+    components: { Head, AppLayoutVue, DateTimePickerM },
+    props: {
+        menu: {
+            type: Object,
+            default: {}
+        },
+    },
+    setup(props, context) {
+        const { menu } = toRefs(props);
+        const permits = usePermissions(menu.value, window.location.pathname);
+        return {
+            permits
+        };
+    },
+}
+</script>
+
+<style>
+.sin-scroll {
+    overflow: auto;
+}
+
+/* Estilos para ocultar la barra de desplazamiento en navegadores webkit */
+.sin-scroll::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+}
+
+/* Estilos para Firefox */
+.sin-scroll {
+    scrollbar-width: none;
+    /* Firefox */
+}
+
+/* Definición de la transición */
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+    transition: opacity 0.3s ease;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+    opacity: 0;
+}
+
+/* Animación de deslizamiento */
+.modal-slide {
+    transform: translateY(0);
+}
+
+.modal-slide-enter,
+.modal-slide-leave-to {
+    transform: translateY(20px);
+    /* Ajusta según tu necesidad */
+}</style>
