f4318518c499ddafda410e8bf73ed2bd0ae9f5be
Segio Lopez <tec2.unifsoporte@isri.gob.sv>
2024-02-08 10:08:41 -0600

    Feat -> commenting code

--- resources/js/Composables/General/usePermissions.js
+++ resources/js/Composables/General/usePermissions.js
@@ -1,27 +1,36 @@
 export const usePermissions = (menu, currentUrl) => {
+    // Función para obtener los permisos
     const getPermissions = () => {
-        let permits = {
+        // Permisos por defecto
+        const defaultPermissions = {
             insertar: 0,
             actualizar: 0,
             eliminar: 0,
             ejecutar: 0,
         };
 
-        menu.urls.forEach((value) => {
-            value.submenu.forEach((value2) => {
-                if (value2.url === currentUrl) {
-                    permits = {
-                        insertar: value2.insertar,
-                        actualizar: value2.actualizar,
-                        eliminar: value2.eliminar,
-                        ejecutar: value2.ejecutar,
-                    };
-                }
-            });
-        });
+        // Bucar el submenu que contiene la URL actual
+        const foundSubmenu = menu.urls.find(value =>
+            value.submenu.some(value2 => value2.url === currentUrl)
+        );
 
-        return permits;
+        if (foundSubmenu) {
+            // Extrae permisos específicos del submenu
+            const { insertar, actualizar, eliminar, ejecutar } = foundSubmenu.submenu.find(value2 => value2.url === currentUrl) || {};
+
+            // Retorna permisos específicos o los permisos por defecto si no se encuentran
+            return {
+                insertar:   insertar    || defaultPermissions.insertar,
+                ejecutar:   ejecutar    || defaultPermissions.ejecutar,
+                eliminar:   eliminar    || defaultPermissions.eliminar,
+                actualizar: actualizar  || defaultPermissions.actualizar,
+            };
+        }
+
+        // Si no se encuentra la URL en el menú, retornar los permisos por defecto
+        return defaultPermissions;
     };
 
+    // Llamar a la función y retornar los permisos
     return getPermissions();
 };
