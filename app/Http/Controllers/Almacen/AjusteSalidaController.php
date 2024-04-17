97a04330110fbaf67dd062adcda133c1703f6dfd
Melvin_hj94 <tec1.unifanalista@isri.gob.sv>
2024-04-17 14:31:15 -0600

    Updating stock for outgoing adjustments

--- app/Http/Controllers/Almacen/AjusteSalidaController.php
+++ app/Http/Controllers/Almacen/AjusteSalidaController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Almacen\AjusteSalidaRequest;
 use App\Models\CentroAtencion;
 use App\Models\DetalleExistenciaAlmacen;
+use App\Models\DetalleKardex;
 use App\Models\DetalleRequerimiento;
 use App\Models\ExistenciaAlmacen;
+use App\Models\Kardex;
 use App\Models\LineaTrabajo;
 use App\Models\MotivoAjuste;
 use App\Models\ProyectoFinanciado;
@@ -342,6 +344,130 @@ class AjusteSalidaController extends Controller
                     'error' => $th->getMessage(),
                 ], 422);
             }
+        }
+    }
+
+    public function changeStatusOutgoingAdjustment(Request $request)
+    {
+        $req = Requerimiento::find($request->id);
+        if ($req->id_estado_req == 1 && $request->status == 1) {
+            DB::beginTransaction();
+            try {
+                $req->update([
+                    'id_estado_req'                         => 4,
+                    'fecha_mod_requerimiento'               => Carbon::now(),
+                    'usuario_requerimiento'                 => $request->user()->nick_usuario,
+                    'ip_requerimiento'                      => $request->ip(),
+                ]);
+                DB::commit(); // Confirma las operaciones en la base de datos
+                return response()->json([
+                    'message'          => "Ajuste eliminado con éxito.",
+                ]);
+            } catch (\Exception $e) {
+                DB::rollBack(); // En caso de error, revierte las operaciones anteriores
+                return response()->json([
+                    'logical_error' => 'Ha ocurrido un error con sus datos.',
+                    'error' => $e,
+                ], 422);
+            }
+        } else {
+            return response()->json(['logical_error' => 'Error, otro usuario ha cambiado el estado de este ajuste.',], 422);
+        }
+    }
+
+    public function sendOutgoingAdjustment(Request $request)
+    {
+        $backEndError = false;
+        //Find the current adjustment
+        $req = Requerimiento::with([
+            'detalles_requerimiento' => function ($query) {
+                $query->where('estado_det_requerimiento', 1);
+            },
+            'detalles_requerimiento.producto'
+        ])->find($request->id);
+        if ($req->id_estado_req == 1) { //We must evaluate if the adjustment has the status 'CREADO'
+            DB::beginTransaction(); //Start the transaction
+            try {
+                //We update the adjustment
+                $req->update([
+                    'id_estado_req'                         => 2, //APROBADO
+                    'fecha_mod_requerimiento'               => Carbon::now(),
+                    'usuario_requerimiento'                 => $request->user()->nick_usuario,
+                    'ip_requerimiento'                      => $request->ip(),
+                ]);
+                //Create a new Kardex object
+                $kardex = new Kardex([
+                    'id_requerimiento'                      => $req->id_requerimiento,
+                    'id_proy_financiado'                    => $req->id_proy_financiado,
+                    'id_tipo_mov_kardex'                    => 2, //SALIDA
+                    'id_tipo_req'                           => 2, //AJUSTE
+                    'fecha_kardex'                          => Carbon::now(),
+                    'fecha_reg_kardex'                      => Carbon::now(),
+                    'usuario_kardex'                        => $request->user()->nick_usuario,
+                    'ip_kardex'                             => $request->ip(),
+                ]);
+                $kardex->save();
+                //Foreach 'detalles_requerimiento' we create a 'detalle_kardex' instance
+                foreach ($req->detalles_requerimiento as $det) {
+                    $detKardex = new DetalleKardex([
+                        'id_kardex'                         => $kardex->id_kardex,
+                        'id_producto'                       => $det->producto->id_producto,
+                        'id_centro_atencion'                => $req->id_centro_atencion,
+                        'id_marca'                          => $det->id_marca,
+                        'id_lt'                             => $req->id_lt,
+                        'cant_det_kardex'                   => $det->cant_det_requerimiento,
+                        'costo_det_kardex'                  => $det->costo_det_requerimiento,
+                        'fecha_vencimiento_det_kardex'      => $det->fecha_vcto_det_requerimiento,
+                        'fecha_reg_det_kardex'              => Carbon::now(),
+                        'usuario_det_kardex'                => $request->user()->nick_usuario,
+                        'ip_det_kardex'                     => $request->ip(),
+                    ]);
+                    $detKardex->save();
+
+                    //Update the stock
+                    $prevStock = DetalleExistenciaAlmacen::find($det->id_det_existencia_almacen);
+                    $newStock = $prevStock->cant_det_existencia_almacen - $det->cant_det_requerimiento; //New specific stock
+                    if ($newStock >= 0) {
+                        $globalStock = ExistenciaAlmacen::find($prevStock->id_existencia_almacen);
+                        //We update the specific stock
+                        $prevStock->update([
+                            'cant_det_existencia_almacen'               => $newStock,
+                            'fecha_mod_det_existencia_almacen'          => Carbon::now(),
+                            'usuario_det_existencia_almacen'            => $request->user()->nick_usuario,
+                            'ip_det_existencia_almacen'                 => $request->ip(),
+                        ]);
+                        //We update the global stock (product)
+                        $globalStock->update([
+                            'cant_existencia_almacen'                   => $globalStock->cant_existencia_almacen - $det->cant_det_requerimiento, //New global stock
+                            'fecha_mod_existencia_almacen'              => Carbon::now(),
+                            'usuario_existencia_almacen'                => $request->user()->nick_usuario,
+                            'ip_existencia_almacen'                     => $request->ip(),
+                        ]);
+                    } else {
+                        $backEndError = true;
+                    }
+                }
+
+                if ($backEndError) {
+                    DB::rollBack(); // En caso de error, revierte las operaciones anteriores
+                    return response()->json([
+                        'logical_error' => 'Uno o más productos de los que estas intentando enviar al kardex no poseen existencias suficientes para procesar la salida, por favor revisa el ajuste nuevamente.',
+                    ], 422);
+                }
+
+                DB::commit(); // Confirma las operaciones en la base de datos
+            } catch (\Exception $e) {
+                DB::rollBack(); // En caso de error, revierte las operaciones anteriores
+                return response()->json([
+                    'logical_error' => 'Ha ocurrido un error con sus datos.',
+                    'error' => $e->getMessage(),
+                ], 422);
+            }
+            return response()->json([
+                'message'          => "Ajuste enviado al Kardex con éxito.",
+            ]);
+        } else {
+            return response()->json(['logical_error' => 'Error, otro usuario ha cambiado el estado de esta donacion.',], 422);
         }
     }
 }
