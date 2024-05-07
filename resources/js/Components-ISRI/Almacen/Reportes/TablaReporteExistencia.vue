08caffbdd421c91155ab47697b482be51d66d83b
Administrator <admin@example.com>
2024-05-07 18:14:47 +0000

    Finish to Report Existencia

---
+++ resources/js/Components-ISRI/Almacen/Reportes/TablaReporteExistencia.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="bg-white">
+        <div>
+            <!-- Table -->
+            <div class="overflow-x-auto">
+                <table class="table-auto w-full">
+                    <!-- Table header -->
+                    <thead
+                        class="font-semibold uppercase text-slate-500 border-t border-b border-slate-200 *:text-xs">
+                        <tr>
+                            <th class="px-2   py-1">
+                                <div class="font-semibold text-left">PROYECTO FINANCIADO</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-left  w-96">PRODUCTO</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">ESPECIFICO</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">MARCA</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">UNIDAD DE MEDIDA</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">CENTRO ATENCION</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">LINEA DE TRABAJO</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">CANTIDAD EXISTENCIA</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">COSTO</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">FECHA VENCIMIENTO</div>
+                            </th>
+                            <th class="px-1  py-1">
+                                <div class="font-semibold text-center">SUB ALMACEN</div>
+                            </th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(item, index) in dataReporteExistencia" :key="index" class="border *:text-xs *:text-center *:hover:bg-slate-200 *:py-1">
+                            <td>{{ item.codigo_proy_rpt_existencia }}</td>
+                            <td style="text-align: left;">{{ item.nombre_prod_rpt_existencia }}</td>
+                            <td>{{ item.id_ccta_presupuesto_rpt_existencia }}</td>
+                            <td>{{ item.marca_rpt_existencia }}</td>
+                            <td>{{ item.nombre_umedida_rpt_existencia }}</td>
+                            <td>{{ item.codigo_centro_rpt_existencia }}</td>
+                            <td>{{ item.codigo_lt_rpt_existencia }}</td>
+                            <td>{{ item.cant_rpt_existencia }}</td>
+                            <td>{{ item.costo_rpt_existencia }}</td>
+                            <td>{{ item.fecha_vcto_rpt_existencia }}</td>
+                            <td>{{ item.id_prod_rpt_existencia }}</td>
+                        </tr>
+                    </tbody>
+
+                    <tbody v-if="isLoadinRequest">
+                        <tr>
+                            <td colspan="12">
+                                <div class="flex items-center justify-center h-96">
+                                    <div aria-label="Loading..." role="status" class="loader">
+                                        <svg class="icon" viewBox="0 0 256 256">
+                                            <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="24"></line>
+                                            <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="24"></line>
+                                            <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="24"></line>
+                                            <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="24"></line>
+                                            <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="24"></line>
+                                            <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="24"></line>
+                                            <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="24"></line>
+                                            <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="24"></line>
+                                        </svg>
+                                        <span class="loading-text">Cargando...</span>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+
+                    <tbody v-if="dataReporteExistencia == '' && isLoadinRequest == false">
+                        <tr>
+                            <td colspan="12">
+                                <div class="w-full h-[500px] px-1 text-selection-disable">
+                                    <div class="flex flex-col items-center justify-center h-full">
+                                        <img src="../../../../img/TableReport.svg" class="h-72 mx-auto" alt="SVG Image"
+                                            draggable="false" />
+                                        <h1 class="font-medium text-center">
+                                            No se encontraron resultados
+                                        </h1>
+                                        <p class="text-[9pt] text-center">
+                                            Intenta buscar en períodos válidos o revisa tus criterios de
+                                            búsqueda.
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { toRefs } from 'vue';
+
+export default {
+    name: "Test",
+    props: ["dataReporteExistencia","isLoadinRequest"],
+    setup() {
+
+
+        return {
+
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.loader {
+    display: flex;
+    align-items: center;
+}
+
+.icon {
+    height: 1.5rem;
+    width: 1.5rem;
+    animation: spin 1s linear infinite;
+    stroke: rgba(107, 114, 128, 1);
+}
+
+.loading-text {
+    font-size: 0.75rem;
+    line-height: 1rem;
+    font-weight: 500;
+    color: rgba(107, 114, 128, 1);
+}
+
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
+}
+</style>
---
+++ resources/js/Composables/Almacen/Reportes/useReporteExistencia.js
@@ -0,0 +1,171 @@
+import { ref, inject, computed, onMounted } from "vue";
+import { createApp, h } from 'vue'
+import { jsPDF } from "jspdf";
+import html2pdf from 'html2pdf.js'
+import moment from "moment";
+import ReporteExistenciaPdf from "@/pdf/Almacen/ReporteExistenciaPdf.vue";
+
+export const useReporteExistencia = () => {
+
+    const dataReporteExistencia = ref([]);
+
+    const idFuenteFinanciamiento = ref(null);
+    const idCentroAtencion = ref(null);
+    const idSubAlmacen = ref(null);
+    const isLoaderRequest = ref(false);
+
+    /**
+     * Función para realizar una búsqueda de reporte de existencia en el almacén.
+     */
+    const searchReportExistencia = async () => {
+        try {
+            isLoaderRequest.value = true
+            // Realiza una solicitud POST para obtener el reporte de existencia en el almacén
+            const resp = await axios.post("/get-reporte-existencia-almacen", {
+                idSubAlmacen: idSubAlmacen.value,
+                idCentroAtencion: idCentroAtencion.value,
+                idFuenteFinanciamiento: idFuenteFinanciamiento.value,
+            });
+
+            // Extrae los datos de la respuesta
+            const { data } = resp;
+
+            // Imprime los datos en la consola (puedes modificar este comportamiento según sea necesario)
+            console.log(data);
+
+            dataReporteExistencia.value = data;
+
+        } catch (error) {
+            // Maneja cualquier error que ocurra
+            console.error('Ocurrió un error al obtener la información del reporte:', error);
+        } finally {
+            isLoaderRequest.value = false;
+            // Código a ejecutar después de la solicitud, independientemente de si fue exitosa o no
+            // Aquí puedes agregar cualquier limpieza de estado o finalización de tareas que necesites realizar
+        }
+    };
+
+    const exportExcel = async () => {
+        try {
+            try {
+                document.body.style.cursor = 'wait';
+
+                const response = await axios.post(
+                    "/get-reporte-existencia-almacen-excel",
+                    {
+                        idSubAlmacen: idSubAlmacen.value,
+                        idCentroAtencion: idCentroAtencion.value,
+                        idFuenteFinanciamiento: idFuenteFinanciamiento.value,
+                    },
+                    { responseType: "blob" }
+                );
+
+                // Crear una URL para el blob
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+
+                // Crear un enlace temporal y simular un clic para descargar el archivo
+                const link = document.createElement("a");
+                link.href = url;
+                link.setAttribute("download", "REPORTE_EXISTENCIA_" + moment().format('L') + ".xlsx"); // Nombre del archivo deseado
+                document.body.appendChild(link);
+                link.click();
+
+                // Liberar la URL del blob después de la descarga
+                window.URL.revokeObjectURL(url);
+                document.body.style.cursor = 'default';
+
+            } catch (error) {
+                console.error('Error al generar el PDF:', error);
+            }
+            finally {
+                //TODO: revisar que el cursos no cambia a default
+                // Restaurar el cursor del cuerpo del documento a "default" después de la generación del PDF
+                document.body.style.cursor = 'default';
+            }
+
+        } catch (error) {
+            console.error("Error al descargar el archivo:", error);
+        }
+    };
+
+
+    const printPdf = () => {
+        document.body.style.cursor = 'wait';
+
+        const opt = {
+            //margin: [0.5, 0.1, 2, 0.5], //top, left, bottom, right,
+            margin: 0.5,
+            filename: "REPORTE_EXISTENCIA_" + moment().format("L"),
+            //pagebreak: { mode: ['avoid-all', 'css', 'legacy'] },
+            image: { type: "jpeg", quality: 0.98 },
+            html2canvas: { scale: 3, useCORS: true },
+            jsPDF: { unit: "cm", format: "letter", orientation: "landscape" },
+        };
+        // Crear una instancia de la aplicación Vue para generar el componente quedanPDFVue
+        const app = createApp(ReporteExistenciaPdf, { dataReporteExistencia: dataReporteExistencia.value });
+        // Crear un elemento div y montar la instancia de la aplicación en él
+        const div = document.createElement("div");
+        const pdfPrint = app.mount(div);
+        const html = div.outerHTML;
+        const currentDateTime = moment().format("DD/MM/YYYY, HH:mm:ss");
+
+        // Generar y guardar el PDF utilizando html2pdf
+        html2pdf()
+            .set(opt)
+            .from(html)
+            .toPdf()
+            .get("pdf")
+            .then(function (pdf) {
+                var totalPages = pdf.internal.getNumberOfPages();
+                for (var i = 1; i <= totalPages; i++) {
+                    pdf.setPage(i);
+                    pdf.setFontSize(10);
+                    //Text for the page number
+                    let text = "Página " + i + " de " + totalPages;
+                    const centerX = pdf.internal.pageSize.getWidth() / 2;
+                    //Get the text width
+                    const textWidth1 =
+                        (pdf.getStringUnitWidth(text) *
+                            pdf.internal.getFontSize()) /
+                        pdf.internal.scaleFactor;
+                    //Get the middle position including the text width
+                    const textX = centerX - textWidth1 / 2;
+                    //Write the text in the desired coordinates.
+                    pdf.text(
+                        textX,
+                        pdf.internal.pageSize.getHeight() - 0.6,
+                        text
+                    );
+                    //Text for the date and time.
+                    let date_text = "Generado: " + currentDateTime;
+                    //Get the text width
+                    const textWidth =
+                        (pdf.getStringUnitWidth(date_text) *
+                            pdf.internal.getFontSize()) /
+                        pdf.internal.scaleFactor;
+                    //Write the text in the desired coordinates.
+                    pdf.text(
+                        pdf.internal.pageSize.getWidth() - textWidth - 0.6,
+                        pdf.internal.pageSize.getHeight() - 0.6,
+                        date_text
+                    );
+                }
+                document.body.style.cursor = 'default';
+
+            })
+            .save()
+            .catch((err) => console.log(err));
+    };
+
+    return {
+        searchReportExistencia,
+        dataReporteExistencia,
+        exportExcel,
+        printPdf,
+        idSubAlmacen,
+        idCentroAtencion,
+        idFuenteFinanciamiento,
+        isLoaderRequest,
+    }
+
+}
