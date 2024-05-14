fef186fee7b1b3a888a106daa83d05d6f9ae316a
Segio Lopez <tec2.unifsoporte@isri.gob.sv>
2024-05-14 14:09:36 -0600

    fix -> fix data printing error in queue document

--- resources/js/pdf/Tesoreria/quedanPDF.vue
+++ resources/js/pdf/Tesoreria/quedanPDF.vue
@@ -147,7 +147,7 @@
                 <td class="border border-b-black border-x-transparent border-t-transparent w-[170px]">
                     <div style="margin-top: -12px;" class="pl-2 pt-2 text-[9pt]">
                         <span style="font-family:  Arial, Helvetica, sans-serif;">{{
-                            dataQuedan.numero_compromiso_ppto_quedan
+                            dataQuedan.numero_doc_adquisicion
                         }}</span>
                     </div>
                 </td>
