c7e59ff29c5a0b73b63774b75d9ecf56cbb15fa4
Segio Lopez <tec2.unifsoporte@isri.gob.sv>
2024-05-16 11:59:57 -0600

    feat -> visual part of the report

--- resources/js/Components-ISRI/RRHH/ModalAcuerdos.vue
+++ resources/js/Components-ISRI/RRHH/ModalAcuerdos.vue
@@ -4,6 +4,7 @@ import 'vue3-toastify/dist/index.css'; // Importar los estilos de vue3-toastify
 import TooltipVue from '../Tooltip.vue';
 import moment from 'moment';
 import Modal from "@/Components-ISRI/AllModal/Modal.vue";
+import { truncateString } from '@/mixins/truncateString';
 
 </script>
 
@@ -170,14 +171,18 @@ import Modal from "@/Components-ISRI/AllModal/Modal.vue";
                                         <div class="sm:flex sm:justify-end sm:items-center pt-4">
                                             <div @click="createNewDeal()"
                                                 class="flex justify-center gap-2 text-[10pt] cursor-pointer hover:border-indigo-400 hover:text-indigo-400  border-2 border-dashed border-indigo-500 text-indigo-500 h-14 w-96 rounded-lg bg-transparent text-center py-4">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                     stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                                     <path stroke-linecap="round" stroke-linejoin="round"
                                                         d="M12 4.5v15m7.5-7.5h-15" />
-                                                </svg>
+                                                </svg> -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+</svg>
+
                                                 <span class="text-selection-disable">{{ dataDeals.some((deal) =>
                                                     deal.isEditingDeal) ?
-                                                    'AGREGAR NUEVA MODIFICACION' : 'AGREGAR NUEVO ACUERDO' }}</span>
+                                                    'CLICK PARA TERMINAR DE MODIFICAR' : 'AGREGAR NUEVO ACUERDO' }}</span>
                                             </div>
                                         </div>
                                         <!--  {{ dataDeals }} -->
@@ -197,9 +202,9 @@ import Modal from "@/Components-ISRI/AllModal/Modal.vue";
                                                             <div class="flex flex-wrap items-center -m-1.5 mx-3 mb-1 gap-1">
                                                                 <span class="inline-flex font-medium">&#8226;{{
                                                                     moment(deal.fecha_acuerdo_laboral).format('L') }}</span>
-                                                                <div :class="deal.color_tipo_acuerdo_laboral"
+                                                                <div :class="deal.color_tipo_acuerdo_laboral" :title="deal.nombre_tipo_acuerdo_laboral"
                                                                     class="inline-flex font-medium  rounded-full text-center px-1.5 py-.5">
-                                                                    {{ deal.nombre_tipo_acuerdo_laboral }}
+                                                                     {{ $options.filters.truncate(deal.nombre_tipo_acuerdo_laboral, 33,'...') }}
                                                                 </div>
                                                             </div>
                                                             <h1 class="mx-3 font-semibold text-justify">
@@ -253,8 +258,8 @@ import Modal from "@/Components-ISRI/AllModal/Modal.vue";
                                     v-if="dataAcuerdos == ''" color="bg-green-700  hover:bg-green-800"
                                     text="Agregar todos los acuerdos" icon="add" />
 
-                                <GeneralButton @click=" updateDeals()" v-else color="bg-orange-700  hover:bg-orange-800"
-                                    text="Editar todos los acuerdos" icon="add" />
+                                <GeneralButton @click=" updateDeals()" v-else color="bg-orange-700  hover:bg-orange-800" class="w-fullP"
+                                    text="Guardar todo y salir" icon="add" />
                             </div>
                         </div>
                     </div>
@@ -308,6 +313,8 @@ import Modal from "@/Components-ISRI/AllModal/Modal.vue";
 
 <script>
 export default {
+    mixins: [truncateString],
+
     props: {
         showModal: {
             type: Boolean,
