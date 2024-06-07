266fb1d2f9a1e7c18e89690be2d4af9f1305e09a
Segio Lopez <tec2.unifsoporte@isri.gob.sv>
2024-06-07 12:13:06 -0600

    style -> changin uppercase

--- resources/js/Components-ISRI/HeaderComponents/UserMenu.vue
+++ resources/js/Components-ISRI/HeaderComponents/UserMenu.vue
@@ -15,7 +15,7 @@
 
             <div class="flex items-center truncate gap-1">
                 <span class="truncate ml-2 text-sm font-medium group-hover:text-slate-800">
-                    CODIGO USUARIO: <span class="font-bold">{{ $page.props.auth.user.nick_usuario }}</span>
+                    codigo usuario: <span class="font-bold">{{ $page.props.auth.user.nick_usuario }}</span>
                 </span>
                 <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400" viewBox="0 0 12 12">
                     <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z"></path>
