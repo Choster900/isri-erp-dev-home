583f6ced0730fdc4d8fa10e966a9201fb1bca6d6
Melvin_hj94 <tec1.unifanalista@isri.gob.sv>
2024-06-07 12:26:34 -0600

    Adding module name

--- resources/js/Pages/Auth/SetPassword.vue
+++ resources/js/Pages/Auth/SetPassword.vue
@@ -43,7 +43,7 @@ const updatePassword = () => {
         <div
             class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden"
         >
-            <HeaderVue />
+            <HeaderVue nameSubModule="Administración" />
 
             <main class="bg-gray-100/50 h-full">
                 <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,7 +9,7 @@ import { Head } from "@inertiajs/vue3";
     <Head title="Dashboard" />
     <div class="flex h-screen overflow-hidden">
         <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-            <HeaderVue />
+            <HeaderVue nameSubModule="Dashboard" />
             <main class="bg-gray-100/50 h-full">
                 <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
                     <div class="flex items-center justify-center">
