f3c7bf1fc77bcf134c3c672511b855bee1576c2a
Melvin_hj94 <tec1.unifanalista@isri.gob.sv>
2024-06-27 10:31:20 -0600

    Fixing bug related to dark mode in label and text input

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -17,8 +17,9 @@ defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
+    <!-- dark classes deleted in input: dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:ring-indigo-600 -->
     <input
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+        class="border-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
