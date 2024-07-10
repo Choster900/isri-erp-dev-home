40479b07d6c1724a417e6cffb6eb81fb04b422f3
Choster900 <16adonaysergio@gmail.com>
2024-07-10 13:34:01 -0600

    feat: update purchase processes and manage employee assignments
    - Enabled updating of purchase processes.
    - Added functionality to add or assign employees to purchase processes in the Warehouse screen.
    - On the UCP screen, you can add processes without assigning employees, regardless of whether they are products or services.

--- app/Http/Controllers/UCP/ProcesoCompraController.php
+++ app/Http/Controllers/UCP/ProcesoCompraController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\UCP;
 
 use App\Http\Controllers\Controller;
+use App\Models\Empleado;
 use App\Models\ProcesoCompra;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -24,11 +25,50 @@ class ProcesoCompraController extends Controller
         $v_dir = $request->input('dir');
         $data = $request->input('search');
 
-        $v_query = ProcesoCompra::with(["empleado.persona","tipo_proceso_compra"])
-        ->orderBy($v_columns[$v_column], $v_dir); // Ajustar $v_columns[$v_column] si column es el índice correcto
+        $v_query = ProcesoCompra::with(["empleado.persona", "tipo_proceso_compra"])
+            ->orderBy($v_columns[$v_column], $v_dir); // Ajustar $v_columns[$v_column] si column es el índice correcto
         if ($data) {
             $v_query->where('id_proceso_compra', 'like', '%' . $data["id_proceso_compra"] . '%');
-               /*  ->where('id_tipo_proceso_compra', 'like', '%' . $data["id_tipo_proceso_compra"] . '%')
+            /*  ->where('id_tipo_proceso_compra', 'like', '%' . $data["id_tipo_proceso_compra"] . '%')
+                ->where('nombre_proceso_compra', 'like', '%' . $data["nombre_proceso_compra"] . '%');
+
+
+            if (isset($data['responsable_sub_almacen'])) {
+                $v_query->whereHas('empleado.persona', function ($query) use ($data) {
+                    $query->whereRaw(
+                        "MATCH (pnombre_persona, snombre_persona, tnombre_persona, papellido_persona, sapellido_persona, tapellido_persona) AGAINST (?)",
+                        [$data['responsable_sub_almacen']]
+                    );
+                });
+            } */
+        }
+
+        $v_requerimientos = $v_query->paginate($v_length)->onEachSide(1);
+        return [
+            'data' => $v_requerimientos,
+            'draw' => $request->input('draw'),
+        ];
+    }
+
+
+    public function getProcesosComprasForAlmacen(Request $request)
+    {
+        $v_columns = [
+            'id_proceso_compra',
+            'id_tipo_proceso_compra',
+            'nombre_proceso_compra',
+        ];
+
+        $v_length = $request->input('length');
+        $v_column = $request->input('column');
+        $v_dir = $request->input('dir');
+        $data = $request->input('search');
+
+        $v_query = ProcesoCompra::with(["empleado.persona", "tipo_proceso_compra"])->where("id_tipo_proceso_compra", 1)
+            ->orderBy($v_columns[$v_column], $v_dir); // Ajustar $v_columns[$v_column] si column es el índice correcto
+        if ($data) {
+            $v_query->where('id_proceso_compra', 'like', '%' . $data["id_proceso_compra"] . '%');
+            /*  ->where('id_tipo_proceso_compra', 'like', '%' . $data["id_tipo_proceso_compra"] . '%')
                 ->where('nombre_proceso_compra', 'like', '%' . $data["nombre_proceso_compra"] . '%');
 
 
@@ -60,7 +100,7 @@ class ProcesoCompraController extends Controller
                 'id_tipo_proceso_compra'    => $request->tipoProcesoCompra,
                 'id_empleado'           => null, // enviamos null por que es en UCP solo se va a poder agregar desde almacen
                 'nombre_proceso_compra'    => $request->nombreProcesoCompra,
-              /*   'fecha_reg_sub_almacen' => Carbon::now(),
+                /*   'fecha_reg_sub_almacen' => Carbon::now(),
                 'usuario_sub_almacen'   => $request->user()->nick_usuario,
                 'ip_sub_almacen'        => $request->ip(), */
             ]);
@@ -92,6 +132,25 @@ class ProcesoCompraController extends Controller
         }
     }
 
+    function updateProcesoCompraEmployee(Request $request)
+    {
+
+        try {
+            DB::beginTransaction();
+            $v_requerimiento = ProcesoCompra::where("id_proceso_compra", $request->idProcesoCompra)->update([
+                'id_empleado'    => $request->idEmpleado,
+                /* 'fecha_mod_sub_almacen' => Carbon::now(),
+                'usuario_sub_almacen'   => $request->user()->nick_usuario,
+                'ip_sub_almacen'        => $request->ip(), */
+            ]);
+            DB::commit();
+            return $v_requerimiento;
+        } catch (\Throwable $th) {
+            DB::rollback();
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+
     function deleteSubAlmacen(Request $request)
     {
 
@@ -108,4 +167,24 @@ class ProcesoCompraController extends Controller
         }
     }
 
+
+    function findEmployeeByName(Request $request)
+    {
+        $empleado = Empleado::whereHas('persona', function ($query) use ($request) {
+            $query->whereRaw(
+                "MATCH (pnombre_persona, snombre_persona, tnombre_persona, papellido_persona, sapellido_persona, tapellido_persona) AGAINST (?)",
+                [$request->nombre]
+            );
+        })->with('persona')->get();
+
+        $formattedResults = $empleado->map(function ($item) {
+            return [
+                'value'           => $item->persona->id_persona,
+                'label'           => $item->persona->nombre_completo,
+                'allDataPersonas' => $item->persona,
+            ];
+        });
+
+        return response()->json($formattedResults);
+    }
 }
--- resources/js/Pages/Almacen/ProcesoCompra.vue
+++ resources/js/Pages/Almacen/ProcesoCompra.vue
@@ -1,18 +1,220 @@
 <template>
 
-    <Head title="Mantenimiento - Proceso Compra" />
-    <AppLayoutVue nameSubModule="Mantenimiento - Proceso Compra" :colorSide="' bg-[#343a40] '">
-            proceso de compra de almacen
+    <Head title="Catalogos - Proceso Compra" />
+    <AppLayoutVue nameSubModule="Catalogos - Proceso Compra" :colorSide="' bg-[#343a40] '">
+
+
+
+
+        <div class="bg-white shadow-lg rounded-sm border border-slate-200 relative">
+            <header class="px-5 py-4">
+                <div class="mb-4 md:flex flex-row justify-items-start">
+                    <div class="mb-4 md:mr-2 md:mb-0 basis-1/4">
+                        <div class="relative flex h-8 w-full flex-row-reverse div-multiselect">
+                            <Multiselect v-model="tableData.length" @select="getDataToShow()" :options="perPage"
+                                :searchable="true" placeholder="Cantidad a mostrar"
+                                @deselect=" tableData.length = 5; getDataToShow()"
+                                @clear="tableData.length = 5; getDataToShow()" />
+                            <LabelToInput icon="list2" />
+                        </div>
+                    </div>
+                    <h2 class="font-semibold text-slate-800 pt-1">
+                        Procesos de compra:
+                        <span class="text-slate-400 font-medium">
+                            {{ tableData.total }}
+                        </span>
+                    </h2>
+                </div>
+            </header>
+            <div class="overflow-x-auto">
+                <Datatable :columns="columns" :sortKey="sortKey" :sortOrders="sortOrders" :searchButton="true"
+                    :sortIcons="true" :staticSelect="false" @sort="sortBy" @datos-enviados="handleData($event)"
+                    @execute-search="getDataToShow()">
+                    <tbody v-if="!isLoadinRequest" class="text-sm divide-y divide-slate-200">
+                        <tr v-for="obj in dataToShow" :key="obj.id_sub_almacen" class="hover:bg-gray-200">
+                            <td class="px-2 first:pl-5 last:pr-5">
+                                <div class="font-medium text-slate-800 flex items-center justify-center min-h-[40px]">
+                                    {{ obj.id_proceso_compra }}
+                                </div>
+                            </td>
+                            <td class="px-2 first:pl-5 last:pr-5">
+                                <div class="font-medium text-slate-800 flex items-center justify-center min-h-[40px]">
+                                    {{ obj.tipo_proceso_compra.nombre_tipo_proceso_compra }}
+                                </div>
+                            </td>
+                            <td class="px-2 first:pl-5 last:pr-5">
+                                <div class="font-medium text-slate-800 flex items-center justify-center min-h-[40px]">
+                                    {{ obj.id_empleado ? obj.empleado.persona.nombre_completo : '-' }}
+                                </div>
+                            </td>
+                            <td class="px-2 first:pl-5 last:pr-5">
+                                <div class="font-medium text-slate-800 flex items-center justify-center min-h-[40px]">
+                                    {{ obj.nombre_proceso_compra }}
+                                </div>
+                            </td>
+                            <td class="px-2 first:pl-5 last:pr-5">
+                                <div class="space-x-1">
+                                    <DropDownOptions>
+
+                                        <div class="flex hover:bg-gray-100 py-1 px-2 rounded cursor-pointer"
+                                            @click.stop="dataToSendModal = obj; modalIsOpen = true">
+                                            <div class="w-8 text-blue-900">
+                                                <span class="text-xs">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+                                                        fill="currentColor" class="size-6">
+                                                        <path fill-rule="evenodd"
+                                                            d="M10.47 2.22a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 1 1-1.06-1.06l.97-.97H5.75a.75.75 0 0 1 0-1.5h5.69l-.97-.97a.75.75 0 0 1 0-1.06Zm-4.94 6a.75.75 0 0 1 0 1.06l-.97.97h5.69a.75.75 0 0 1 0 1.5H4.56l.97.97a.75.75 0 1 1-1.06 1.06l-2.25-2.25a.75.75 0 0 1 0-1.06l2.25-2.25a.75.75 0 0 1 1.06 0Z"
+                                                            clip-rule="evenodd" />
+                                                    </svg>
+
+                                                </span>
+                                            </div>
+                                            <div class="font-semibold text-xs pl-2">Asignar o cambiar empleado a proceso
+                                                de compra
+                                            </div>
+                                        </div>
+                                    </DropDownOptions>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody v-else>
+                        <tr>
+                            <td colspan="8" class="text-center">
+                                <img src="../../../img/IsSearching.gif" alt="" class="w-60 h-60 mx-auto">
+                                <h1 class="font-medium text-xl mt-4">Cargando!!!</h1>
+                                <p class="text-sm text-gray-600 mt-2 pb-10">Por favor espera un momento mientras se
+                                    carga la
+                                    información.</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody v-if="emptyObject && !isLoadinRequest">
+                        <tr>
+                            <td colspan="8" class="text-center">
+                                <img src="../../../img/NoData.gif" alt="" class="w-60 h-60 mx-auto">
+                                <h1 class="font-medium text-xl mt-4">No se encontraron resultados!</h1>
+                                <p class="text-sm text-gray-600 mt-2 pb-10">Parece que no hay registros disponibles en
+                                    este
+                                    momento.</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Datatable>
+
+            </div>
+
+            <div v-if="!emptyObject" class="px-6 py-4 bg-white shadow-lg rounded-sm border-slate-200 relative">
+                <div>
+                    <nav class="flex justify-between" role="navigation" aria-label="Navigation">
+                        <div class="grow text-center">
+                            <ul class="inline-flex text-sm font-medium -space-x-px">
+                                <li v-for="link in links" :key="link.label">
+                                    <span v-if="link.label === 'Anterior'"
+                                        :class="(link.active ? 'inline-flex items-center justify-center rounded-full leading-5 px-2 py-2 bg-white border border-slate-200 text-indigo-500 shadow-sm' : 'inline-flex items-center justify-center leading-5 px-2 py-2 text-slate-600 hover:text-indigo-500 border border-transparent')">
+                                        <a @click="getDataToShow(link.url)"
+                                            class="btn bg-white border-slate-200 hover:border-slate-300 cursor-pointer text-indigo-500">
+                                            <div class="flex items-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                                                </svg>
+                                                <span class="hidden sm:inline ml-1">Anterior</span>
+                                            </div>
+                                        </a>
+                                    </span>
+                                    <span v-else-if="(link.label == 'Siguiente')"
+                                        :class="(link.active ? 'inline-flex items-center justify-center rounded-full leading-5 px-2 py-2 bg-white border border-slate-200 text-indigo-500 shadow-sm' : 'inline-flex items-center justify-center leading-5 px-2 py-2 text-slate-600 hover:text-indigo-500 border border-transparent')">
+                                        <div class="flex-1 text-right ml-2">
+                                            <a @click="getDataToShow(link.url)"
+                                                class=" btn bg-white border-slate-200 hover:border-slate-300 cursor-pointer text-indigo-500">
+
+                                                <div class="flex items-center">
+                                                    <span class="hidden sm:inline">Siguiente&nbsp;</span> <svg
+                                                        xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                                                    </svg>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </span>
+                                    <span class="cursor-pointer mt-3" v-else @click="getDataToShow(link.url)"
+                                        :class="(link.active ? 'inline-flex items-center justify-center rounded-full leading-5 px-2 py-2 bg-white border border-slate-200 text-indigo-500 shadow-sm' : 'inline-flex items-center justify-center leading-5 px-2 py-2 text-slate-600 hover:text-indigo-500 border border-transparent')"><span
+                                            class=" w-5">{{ link.label }}</span>
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+
+        </div>
+
+
+        <ModalProcesoCompra @close-modal="modalIsOpen = false" :ModalIsOpen="modalIsOpen"
+            :dataProcesoCompra="dataToSendModal" @actualizar-datatable="getDataToShow(); modalIsOpen = false" />
+
+
     </AppLayoutVue>
 
 </template>
 
 <script>
+import { usePermissions } from '@/Composables/General/usePermissions.js';
+import { toRefs, ref } from 'vue';
+import { useToDataTable } from '@/Composables/General/useToDataTable.js';
+import Datatable from '@/Components-ISRI/Datatable.vue';
+import IconM from '@/Components-ISRI/ComponentsToForms/IconM.vue';
+import ModalProcesoCompra from "../../Components-ISRI/Almacen/Mantenimiento/ModalProcesoCompra.vue"
 export default {
+    components: { Datatable, IconM, ModalProcesoCompra },
+    props: {
+        menu: { type: Object, default: () => ({}) },
+    },
+    setup(props) {
 
-    setup() {
+        const { menu } = toRefs(props);
+        const permits = usePermissions(menu.value, window.location.pathname);
+        const modalIsOpen = ref(false);
+        const dataToSendModal = ref(null);
 
-        return {}
+
+
+        const columns = [
+            { width: "5%", label: "Id", name: "id_proceso_compra", type: "text" },
+            { width: "5%", label: "Tipo Proceso", name: "id_tipo_proceso_compra", type: "text" },
+            { width: "13%", label: "Responsable", name: "id_empleado", type: "text" },
+            { width: "14%", label: "Nombre de proceso", name: "id_empleado", type: "text" },
+            { width: "1%", label: "Acciones", name: "Acciones" },
+        ];
+
+        const requestUrl = "/get-proceso-compra-for-almacen"
+        const columntToSort = "id_proceso_compra"
+        const dir = 'desc'
+
+        const {
+            links, sortKey, sortOrders,
+            isLoadinRequest, emptyObject,
+            dataToShow, tableData, perPage,
+            getDataToShow, handleData, sortBy
+        } = useToDataTable(columns, requestUrl, columntToSort, dir)
+
+
+        return {
+            columns, dataToSendModal,
+            links, sortKey, sortOrders,
+            isLoadinRequest, emptyObject,
+            dataToShow, tableData, perPage,
+            modalIsOpen,
+            getDataToShow, handleData, sortBy
+
+
+        }
     }
 }
 </script>
