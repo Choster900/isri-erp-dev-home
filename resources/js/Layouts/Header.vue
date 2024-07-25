0403b8d7f4c9b98134506601804ebe13e32ff972
Choster900 <16adonaysergio@gmail.com>
2024-07-25 10:33:13 -0600

    feat: Refactor and optimize sidebar handling
    - Rename variables for better clarity:
    - Improve sidebar and modal visibility management.
    - Ensure sidebar and modal visibility logic is clearer and more efficient.

--- resources/js/Layouts/Header.vue
+++ resources/js/Layouts/Header.vue
@@ -8,26 +8,32 @@ import UserMenuVue from '@/Components-ISRI/HeaderComponents/UserMenu.vue';
 import Notifications from '@/Components-ISRI/HeaderComponents/Notifications.vue';
 import Help from '@/Components-ISRI/HeaderComponents/Help.vue';
 
-// Estado local para el modal de búsqueda
-const stateLocal = ref(false);
+// @State: Estado local para controlar la visibilidad del modal de búsqueda
+const isSearchModalOpen = ref(false);
 
-// Recibir props
+// @Props: Definición de las propiedades recibidas del componente padre
 const props = defineProps({
-    nameSubModule: {
+    moduleName: {
         type: String,
-        default: '',
-        required: true,
-    }
+        default: '', // Valor predeterminado si no se proporciona
+        required: true, // Propiedad obligatoria para el nombre del submódulo
+    },
+    isBackgroundVisible: {
+        type: Boolean,
+        default: true, // Valor predeterminado si no se proporciona
+        required: true, // Propiedad obligatoria para la visibilidad del fondo
+    },
 });
 </script>
 <template>
-    <header class="sticky top-0 bg-white border-b border-slate-200 z-30">
+    <header class="sticky top-0 bg-white border-b border-slate-200 z-30 " id="header-content"
+        :class="{ 'modal-open': isBackgroundVisible }">
         <div class="px-4 sm:px-6 lg:px-8">
             <div class="flex items-center justify-between h-16 -mb-px">
                 <!-- Header: Left side -->
                 <div class="flex">
                     <!-- Hamburger button -->
-                    <button class="text-slate-500 hover:text-slate-600 lg:hidden" @click="$emit('OpenOrCloseModal')"
+                    <button class="text-slate-500 hover:text-slate-600 lg:hidden" @click="$emit('OpenSidebarModal')"
                         aria-controls="sidebar">
                         <span class="sr-only">Open sidebar</span>
                         <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -38,13 +44,13 @@ const props = defineProps({
                     </button>
 
                     <h1 class="font-semibold text-base text-slate-800 pt-1">
-                        <span id="text-submodule" class="truncate">{{ nameSubModule }}</span>
+                        <span id="text-submodule" class="truncate">{{ moduleName }}</span>
                     </h1>
                 </div>
 
                 <!-- Header: Right side -->
                 <div class="flex items-center space-x-3">
-                    <!-- <button @click="stateLocal = !stateLocal"
+                    <!-- <button @click="isSearchModalOpen  = !isSearchModalOpen "
                         class="w-8 h-8 flex items-center justify-center bg-slate-100 hover:bg-slate-200 transition duration-150 rounded-full ml-3"
                         aria-controls="search-modal">
                         <span class="sr-only">Search</span>
@@ -66,7 +72,7 @@ const props = defineProps({
                 </div>
             </div>
         </div>
-        <SearchModal :stateToModalSearch="stateLocal" />
+        <SearchModal :stateToModalSearch="isSearchModalOpen" />
     </header>
 </template>
 <style>
@@ -80,23 +86,38 @@ const props = defineProps({
 /* Para pantallas pequeñas (móviles) */
 @media (max-width: 639px) {
     #text-submodule {
-        max-width: 100px; /* Ajusta este valor según tus necesidades */
+        max-width: 100px;
+        /* Ajusta este valor según tus necesidades */
     }
 }
 
 /* Para pantallas medianas (tabletas) */
 @media (min-width: 640px) and (max-width: 1023px) {
     #text-submodule {
-        max-width: 400px; /* Ajusta este valor según tus necesidades */
+        max-width: 400px;
+        /* Ajusta este valor según tus necesidades */
     }
 }
 
 /* Para pantallas grandes (escritorios) */
 @media (min-width: 1024px) {
     #text-submodule {
-        max-width: 400px; /* Ajusta este valor según tus necesidades */
+        max-width: 400px;
+        /* Ajusta este valor según tus necesidades */
     }
 }
 
 
+@media (max-width: 1023px) {
+    .modal-open::before {
+        content: "";
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        z-index: 10;
+    }
+}
 </style>
